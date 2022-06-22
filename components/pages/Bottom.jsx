d0e2eeb45699e8a45dcd7270c667c6583de3460a
xiangkeguo <xiangkeguo@gmail.com>
2022-06-22 15:27:31 +0800

    Error: Using target="_blank" without rel="noreferrer" is a security risk: see https://html.spec.whatwg.org/multipage/links.html\#link-type-noopener  react/jsx-no-target-blank

--- components/pages/Bottom.jsx
+++ components/pages/Bottom.jsx
@@ -9,7 +9,7 @@ const Bottom = () => {
         <IonImg src="https://ly.cdn.yongbuzhixi.com/images/ly_logo.png" />
       </div>
       <div className="flex justify-center text-gray-500 mt-0 mb-1 ">
-        <p className=""><a target="_blank" href="https://729ly.net">网址：729ly.net</a></p>
+        <p className=""><a rel="noreferrer" target="_blank" href="https://729ly.net">网址：729ly.net</a></p>
       </div>
     </div>
   );
