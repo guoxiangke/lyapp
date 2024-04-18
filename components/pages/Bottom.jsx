c519844466f88bcfb4bc6c78805bb7db8446f200
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2024-04-17 22:42:51 -0700

    1949ly.net

--- components/pages/Bottom.jsx
+++ components/pages/Bottom.jsx
@@ -6,10 +6,10 @@ const Bottom = () => {
   return (
     <div>
       <div className="flex justify-center text-gray-400 mt-4">
-        <IonImg src="https://729ly.net/images/ly_logo.png" />
+        <IonImg src="https://1949ly.net/images/ly_logo.png" />
       </div>
       <div className="flex justify-center text-gray-500 mt-0 mb-1 ">
-        <p className=""><a rel="noreferrer" target="_blank" href="https://729ly.net">网址：729ly.net</a></p>
+        <p className=""><a rel="noreferrer" target="_blank" href="https://1949ly.net">网址：1949ly.net</a></p>
       </div>
     </div>
   );
