82e28560ded5eeda3197aff381fe96f0bf0e0dd1
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2024-05-03 07:48:14 -0700

    add logo remove link

--- components/pages/Bottom.jsx
+++ components/pages/Bottom.jsx
@@ -6,10 +6,7 @@ const Bottom = () => {
   return (
     <div>
       <div className="flex justify-center text-gray-400 mt-4">
-        <IonImg src="https://1949ly.net/images/ly_logo.png" />
-      </div>
-      <div className="flex justify-center text-gray-500 mt-0 mb-1 ">
-        <p className=""><a rel="noreferrer" target="_blank" href="https://1949ly.net">网址：1949ly.net</a></p>
+        <IonImg className="w-24" src="/ly_logo.png" alt="良友电台" title="良友电台" />
       </div>
     </div>
   );
