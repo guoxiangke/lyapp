b6736c31b144bf1304eeb0d59eeb21f399d9ff67
me@mbp.m3 <TWR-****-****-****-7F47>
2024-12-11 21:21:08 -0800

    update url

--- components/pages/Bottom.jsx
+++ components/pages/Bottom.jsx
@@ -6,7 +6,9 @@ const Bottom = () => {
   return (
     <div>
       <div className="flex justify-center text-gray-400 mt-4">
-        <IonImg className="w-24" src="/ly_logo.png" alt="良友电台" title="良友电台" />
+        <a href={process.env.officialUrl} rel="noreferrer" target="_blank">
+          <IonImg className="w-24" src="/ly_logo.png" alt="良友电台" title="良友电台" />
+        </a>
       </div>
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
   env: {
     apiUrl: 'https://y.lydt.work',
     bannersUrl: 'https://d3ml8yyp1h3hy5.cloudfront.net/ly/image/cover/[code].jpg',
-    txChannelUrl: 'https://lyapp2.net/zh-sc/listen/channel/channel-tx',
+    txChannelUrl: 'https://ly1.zyqstx.net/zh-sc/listen/channel/channel-tx',
+    officialUrl: 'https://lyyydt.net',
   }
 };
