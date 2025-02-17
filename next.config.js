f7db43b617875540aed5eb2eb406edc52d63252f
me@mbp.m3 <TWR-****-****-****-7F47>
2025-02-16 20:15:13 -0800

    tx2 url

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
   env: {
     apiUrl: 'https://y.lydt.work',
     bannersUrl: 'https://d3ml8yyp1h3hy5.cloudfront.net/ly/image/cover/[code].jpg',
-    txChannelUrl: 'https://ly1.zyqstx.net/zh-sc/listen/channel/channel-tx',
+    txChannelUrl: 'https://ly2.zyqstx.net/zh-sc/listen/channel/channel-tx',
     officialUrl: 'https://lyyydt.net',
   }
 };
