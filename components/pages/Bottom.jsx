84e2cdfe846aa6f1d3acb2cc3eef6193d6af956a
me@mbp.m3 <TWR-****-****-****-7F47>
2025-11-24 08:55:53 -0800

    logo=>txt

--- components/pages/Bottom.jsx
+++ components/pages/Bottom.jsx
@@ -7,7 +7,7 @@ const Bottom = () => {
     <div>
       <div className="flex justify-center text-gray-400 mt-4">
         <a href={process.env.officialUrl} rel="noreferrer" target="_blank">
-          <IonImg className="w-24" src="/ly_logo.png" alt="良友电台" title="良友电台" />
+          <p>良友随身听</p>
         </a>
       </div>
     </div>
